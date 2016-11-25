793c90e1c28f25ae91be9ef0b16d6474ce2684c1
_nullbot <sonderblohm.dev@gmail.com>
2016-11-25 01:49:30 +0100

    [BE] Added Position Report

---
+++ Source/BuildingEscape/PositionReport.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "PositionReport.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UPositionReport : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UPositionReport();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+		
+	
+};
