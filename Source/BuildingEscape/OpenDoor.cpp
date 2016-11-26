1e0e856140e64f9ae83b07ca4e133a8f3d566d40
_nullbot <sonderblohm.dev@gmail.com>
2016-11-26 02:34:12 +0100

    [BE] Adding Actor components to access Owner properties

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,42 @@
+// Erick Pombo Sonderblohm 2016
+
+#include "BuildingEscape.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	AActor* Owner = GetOwner();
+	FQuat Rotation = Owner->GetTransform().GetRotation();
+	FRotator NewRotation = FRotator(0.f, 60.f, 0.f);
+
+	UE_LOG(LogTemp, Warning, TEXT("rotation before change is %s"), *Owner->GetTransform().GetRotation().ToString());
+	Owner->SetActorRotation(NewRotation);
+	UE_LOG(LogTemp, Warning, TEXT("new rotation is %s"), *Owner->GetTransform().GetRotation().ToString());
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
--- Source/BuildingEscape/PositionReport.cpp
+++ Source/BuildingEscape/PositionReport.cpp
@@ -8,8 +8,6 @@ UPositionReport::UPositionReport()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -18,8 +16,11 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	FString Objectname = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
+
+	// Log = grey, Warning = yellow, Error = red
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *Objectname, *ObjectPos);
 }
 
 
